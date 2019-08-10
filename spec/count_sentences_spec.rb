87724ae1a6ddb419d24a924f3535559703ea539f
Patrick Harrison <jahman@bellatlantic.net>
2019-08-10 14:36:50 +0000

    Automatically backed up by Learn

--- spec/count_sentences_spec.rb
+++ spec/count_sentences_spec.rb
@@ -38,9 +38,10 @@ describe String do
     it  "returns the number of sentences in a string" do
       expect("one. two. three?".count_sentences).to eq(3)
     end
-binding.pry
+
     it "returns zero if there are no sentences in a string" do
       expect("".count_sentences).to eq(0)
+      binding.pry
     end
 
     it "returns the number of sentences in a complex string" do
